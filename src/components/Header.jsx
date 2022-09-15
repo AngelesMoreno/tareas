94a4dd46447e7acff5e97e34e26085935790e692
A Moreno <oomakegeos@gmail.com>
2022-09-15 10:28:38 -0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = ({titulo}) => {
+    return (
+        <header>
+            {/*usando props <h1>{props.titulo}</h1>*/}
+            <h1>{titulo}</h1>
+
+        </header>
+        
+    )
+}
+
+export default Header
